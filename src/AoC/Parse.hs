4ef41046b3eb91cd085a92ce7f18539850cc6bd1
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-11-30 18:03:13 +0100

    Add parsing helpers to parse test input

--- src/AoC/Parse.hs
+++ src/AoC/Parse.hs
@@ -3,6 +3,13 @@
 -- Description : Utility functions for parsing problems and their inputs.
 module AoC.Parse where
 
+import Control.Monad (void)
+import Data.Bifunctor (Bifunctor (bimap))
+import Data.Functor (($>))
+import Data.Void (Void)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
 import Text.Printf (printf)
 
 -- | Returns the name of the input file for a given day.
@@ -20,3 +27,27 @@ exampleName ::
   Char ->
   FilePath
 exampleName = printf "inputs/day%02d%c.txt"
+
+type Parser = Parsec Void String
+
+number :: Integral a => Parser a
+number = L.signed (return ()) L.decimal
+
+exampleToNum :: (String, String) -> (Int, Int)
+exampleToNum = bimap read read
+
+pExamples :: Parser [(String, String)]
+pExamples = some pExample <* eof
+
+pExample :: Parser (String, String)
+pExample = do
+  input <- concat <$> some pInput
+  answer <- pAnswer
+  pure (input, answer)
+  where
+    pInput = do
+      notFollowedBy (chunk ">>>" <|> eof $> "")
+      takeWhile1P Nothing (/= '\n') <> (eol <|> eof $> "")
+    pAnswer = do
+      void (chunk ">>>")
+      takeWhile1P Nothing (/= '\n') <* (eol <|> eof $> "")
